dc76c1c226c23408d461aeb038d70d2fc62324c9
Minhajul Anwar <minhaj.me.bd@gmail.com>
2022-01-18 18:43:41 +0600

    V0.9

--- README.md
+++ README.md
@@ -43,7 +43,6 @@ The object provides methods that you need to use are:
 - Then set error handlers, checkout callback
 - Then call the checkout method
 
-
 At first, you need to configure the object. The minimum config/environment variables that your site need to maintain(config related to shurjopay) are:
 
 ```json
@@ -256,6 +255,107 @@ router.post('/checkout_action', (req, res, next) => {
 });
 
 module.exports = router;
+
+```
+
+### Response format examples
+
+#### verify
+
+```javascript
+response_data =
+    [
+        {
+            id: 3741,
+            order_id: 'sp61e678dd003c6',
+            currency: 'BDT',
+            amount: 500,
+            payable_amount: 500,
+            discsount_amount: null,
+            disc_percent: 0,
+            usd_amt: 0,
+            usd_rate: 0,
+            card_holder_name: null,
+            card_number: null,
+            phone_no: '01534303074',
+            bank_trx_id: '61e678eb',
+            invoice_no: 'sp61e678dd003c6',
+            bank_status: 'Success',
+            customer_order_id: 'c4xyxpytzk00',
+            sp_code: 1000,
+            sp_massage: 'Success',
+            name: 'Minhajul Anwar',
+            email: null,
+            address: '330 NIH BUT DHK',
+            city: 'Dhaka',
+            value1: null,
+            value2: null,
+            value3: null,
+            value4: null,
+            transaction_status: null,
+            method: 'Nagad',
+            date_time: '2022-01-18 14:23:07'
+        }
+    ]
+
+```
+
+#### check_status
+
+```javascript
+[
+    {
+        id: 3754,
+        order_id: 'sp61e69482835b5',
+        currency: 'BDT',
+        amount: 500,
+        payable_amount: 500,
+        discsount_amount: null,
+        disc_percent: 0,
+        usd_amt: 0,
+        usd_rate: 0,
+        card_holder_name: null,
+        card_number: null,
+        phone_no: '01534303074',
+        bank_trx_id: '61e6948f',
+        invoice_no: 'sp61e69482835b5',
+        bank_status: 'Success',
+        customer_order_id: 'dvijbs4f5s00',
+        sp_code: 1000,
+        sp_massage: 'Success',
+        name: 'Minhajul Anwar',
+        email: null,
+        address: '330 NIH BUT DHK',
+        city: 'Dhaka',
+        value1: null,
+        value2: null,
+        value3: null,
+        value4: null,
+        transaction_status: null,
+        method: 'Nagad',
+        date_time: '2022-01-18 16:21:03'
+    }
+]
+```
+
+#### checkout
+
+```javascript
+response_data = {
+    checkout_url: 'https://sandbox.securepay.shurjopayment.com/spaycheckout/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvc2FuZGJveC5zaHVyam9wYXltZW50LmNvbVwvYXBpXC9sb2dpbiIsImlhdCI6MTY0MjQ5NTk3MiwiZXhwIjoxNjQyNDk5NTcyLCJuYmYiOjE2NDI0OTU5NzIsImp0aSI6Im1JcEFrNHJPZ1h4TklWVG4iLCJzdWIiOjEsInBydiI6IjgwNWYzOWVlZmNjNjhhZmQ5ODI1YjQxMjI3ZGFkMGEwNzZjNDk3OTMifQ.k_RnbXwWIEc8_NiGgR3c3d0GQhASXv_fjK2S_Wz_Ksw&order_id=sp61e67fe5a7a17',
+    amount: 500,
+    currency: 'BDT',
+    sp_order_id: 'sp61e67fe5a7a17',
+    customer_order_id: 'aobws09sa800',
+    customer_name: 'Minhajul Anwar',
+    customer_address: '330 NIH BUT DHK',
+    customer_city: 'Dhaka',
+    customer_phone: '01534303074',
+    customer_email: null,
+    client_ip: 'unknown',
+    intent: 'sale',
+    transactionStatus: 'Initiated'
+}
 
 ```
 
--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const axios = require('axios');
 const moment = require('moment');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+const logger = createLogger({
+    format: combine(
+        label({ label: 'shurjopaynode' }),
+        timestamp(),
+        myFormat
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({ filename: 'shurjopaynode.log' })
+    ]
+});
 
 function randomString(length){
     return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
@@ -8,11 +25,18 @@ function randomString(length){
 function SP(){
     const _this = this;
 
-    this.session = null;
+    this.data = {};
 
     this.settings = require('./settings');
 
     this.randomString = randomString;
+
+    this.log = (message, level) => {
+        logger.log({
+            level: level,
+            message: message
+        });
+    };
 
     this.configure_merchant = function (merchant_username, merchant_password, merchant_store_id, merchant_key_prefix, merchant_default_currency){
         this.settings.merchant_username = merchant_username;
@@ -27,84 +51,86 @@ function SP(){
     this.checkout_error_handler = function (error_message){
     };
 
-    this.checkout_callback = function (checkout_url, response_data){
-    };
-
-    this.checkout = function (checkout_params){
+    this.getToken = function (callback){
         //  get token first
         axios.post(this.settings.token_url,
             { username: this.settings.merchant_username, password: this.settings.merchant_password })
             .then(function (response){
-                _this.session.sp_token = {
+                _this.data.sp_token = {
                     token: response.data.token,
                     token_type: response.data.token_type,
                     token_create_time: response.data.TokenCreateTime, //eg. 2022-01-13 10:55:12am
                     token_valid_duration: response.data.expires_in ////eg. 3600
                 };
-
-                checkout_params.token = response.data.token;
-                checkout_params.execute_url = response.data.execute_url;
-
-                // now do the checkout
-                axios.post(checkout_params.execute_url, {
-                    prefix: _this.settings.merchant_key_prefix,
-                    token: _this.session.sp_token.token,
-                    return_url: _this.settings.checkout_return_url,
-                    cancel_url: _this.settings.checkout_cancel_url,
-                    store_id: _this.settings.merchant_store_id,
-                    amount: checkout_params.amount,
-                    order_id: checkout_params.order_id,
-                    currency: checkout_params.currency || _this.settings.merchant_default_currency,
-                    customer_name: checkout_params.customer_name,
-                    customer_address: checkout_params.customer_address,
-                    customer_phone: checkout_params.customer_phone,
-                    customer_city: checkout_params.customer_city,
-                    customer_post_code: checkout_params.customer_post_code,
-                    client_ip: checkout_params.client_ip
-                }).then(function (response){
-                    _this.checkout_callback(response.data.checkout_url, response.data);
-                }).catch(function (checkout_error){
-                    _this.checkout_error_handler(checkout_error);
-                });
+                callback(response.data, response.data.token, response.data.token_type, response.data.TokenCreateTime, response.data.expires_in);
             }).catch(function (getToken_error){
             _this.gettoken_error_handler(getToken_error);
         });
     };
 
-    this.verify = function (order_id, callback){
-        axios({
-            method: 'post',
-            url: this.settings.verification_url,
-            headers: { 'content-type': 'application/json', 'Authorization': _this.session.sp_token.token_type + ' ' + _this.session.sp_token.token },
-            data: { order_id: order_id }
-        })
-            .then(function (response){
-                callback(response.data);
-            })
-            .catch(function (verify_error){
-
+    this.checkout = function (checkout_params, checkout_callback){
+        this.getToken((data, token) => {
+            axios.post(data.execute_url, {
+                prefix: _this.settings.merchant_key_prefix,
+                store_id: _this.settings.merchant_store_id,
+                token: token,
+                return_url: checkout_params.return_url,
+                cancel_url: checkout_params.cancel_url,
+                amount: checkout_params.amount,
+                order_id: checkout_params.order_id,
+                currency: checkout_params.currency || _this.settings.merchant_default_currency,
+                customer_name: checkout_params.customer_name,
+                customer_address: checkout_params.customer_address,
+                customer_phone: checkout_params.customer_phone,
+                customer_city: checkout_params.customer_city,
+                customer_post_code: checkout_params.customer_post_code,
+                client_ip: checkout_params.client_ip
+            }).then(function (response){
+                checkout_callback(response.data, response.data.checkout_url);
+            }).catch(function (checkout_error){
+                _this.checkout_error_handler(checkout_error);
             });
+        });
     };
 
-    this.check_status = function (order_id, callback){
-        axios({
-            method: 'post',
-            url: this.settings.payment_status_url,
-            headers: { 'content-type': 'application/json', 'Authorization': _this.session.sp_token.token_type + ' ' + _this.session.sp_token.token },
-            data: { order_id: order_id }
-        })
-            .then(function (response){
-                callback(response.data);
+    this.verify = function (order_id, callback, error_handler){
+        this.getToken((data, token, token_type) => {
+            axios({
+                method: 'post',
+                url: this.settings.verification_url,
+                headers: { 'content-type': 'application/json', 'Authorization': token_type + ' ' + token },
+                data: { order_id: order_id }
             })
-            .catch(function (verify_error){
+                .then(function (response){
+                    callback(response.data);
+                })
+                .catch(function (verify_error){
+                    error_handler(verify_error);
+                });
+        });
+    };
 
-            });
+    this.check_status = function (order_id, callback, error_handler){
+        this.getToken((data, token, token_type) => {
+            axios({
+                method: 'post',
+                url: this.settings.payment_status_url,
+                headers: { 'content-type': 'application/json', 'Authorization': token_type + ' ' + token },
+                data: { order_id: order_id }
+            })
+                .then(function (response){
+                    callback(response.data);
+                })
+                .catch(function (verify_error){
+                    error_handler(verify_error);
+                });
+        });
     };
 
     this.token_valid = function (){
-        let create_time_obj = moment(_this.session.sp_token.token_create_time, 'YYYY-MM-DD hh:mm:ssa');
+        let create_time_obj = moment(_this.data.sp_token.token_create_time, 'YYYY-MM-DD hh:mm:ssa');
         let dur_seconds = moment().subtract(create_time_obj).format('ss');
-        return dur_seconds < _this.session.sp_token.token_valid_duration;
+        return dur_seconds < _this.data.sp_token.token_valid_duration;
     };
 }
 
